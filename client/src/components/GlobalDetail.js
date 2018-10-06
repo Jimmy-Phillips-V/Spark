0a9688b3869cea66302b005cb2d5cca3378f036b
Andrew Norris <andrewmnorris@gmail.com>
2018-10-06 14:19:08 -0700

    trying to map through out res object

---
+++ client/src/components/GlobalDetail.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const GlobalDetail = props => (
+    <div className="jumbotron">
+        <h4>
+        Name: {props.name}
+        </h4>
+
+
+    
+
+    </div>
+
+);
+export default GlobalDetail;

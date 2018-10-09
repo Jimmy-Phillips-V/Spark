2d6686b349f9b0b27b0a4718ee5653e120251176
James Albert Phillips V <jamesalbertphillipsv@airbears2-10-142-153-85.airbears2.1918.berkeley.edu>
2018-10-09 11:11:13 -0700

    revenge of the integration

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,13 @@
 const router = require("express").Router();
+const logRoutes = require("./login");
 const globalRoutes = require("./global");
 const natRoutes = require("./national");
 const localRoutes = require("./local")
 
 // routes
 router.use("/global", globalRoutes);
+
+router.use("/login", logRoutes);
 
 router.use("/national", natRoutes);
 

0e238b1144ae2abeaf9e447b05e0573ecbfe9bae
Andrew Norris <andrewmnorris@gmail.com>
2018-10-14 19:23:47 -0700

    changed headers

--- client/src/components/JumbotronGlobal/Jumbotron-global.js
+++ client/src/components/JumbotronGlobal/Jumbotron-global.js
@@ -3,7 +3,7 @@ import "./Jumbotron.css";
 
 export const JumbotronGlobal = () => (
   <div id="global" className="jumbotron my-3">
-    <span class="span"><h4>Find the charities best suited to address these global crises</h4></span>
+    <h2 id="national-title">Global</h2>  <span class="span"><h4>Find the charities best suited to address these global incidents</h4></span>
   </div>
 );
 
